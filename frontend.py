45359fd55dbf26bf60af2cc20c69a424c4587f23
SpartificialUdemy <instructors@spartificial.com>
2024-08-14 03:13:16 +0530

    added page configuration

--- frontend.py
+++ frontend.py
@@ -4,6 +4,8 @@ import streamlit as st
 import requests
 from utils import preprocess_image
 
+st.set_page_config(page_title = "Segment Moon", page_icon = ":moon:")
+
 # URL of your FastAPI backend endpoint for image segmentation
 FASTAPI_URL = "http://localhost:8000/segment/"
 
